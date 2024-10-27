44288cdb0a6c658d63f28eb0aaa06e87e4cc502a
Martin Bauernschmitt <martin.bauernschmitt@posteo.de>
2024-10-27 12:39:00 +0100

    Modified namespace for camera nodes

--- launch/webcam.launch.py
+++ launch/webcam.launch.py
@@ -5,7 +5,7 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='v4l2_camera',
-            namespace='',
+            namespace='cam1',
             executable='v4l2_camera_node',
             name='v4l2_camera_node1',
             output="screen",
@@ -15,14 +15,11 @@ def generate_launch_description():
                     'video_device': '/dev/video0'
                 }
             ],
-            remappings=[
-                ('image', 'image_raw1'),
-            ],
             arguments=['--ros-args', '--log-level', 'info']
         )
         Node(
             package='v4l2_camera',
-            namespace='',
+            namespace='cam2',
             executable='v4l2_camera_node',
             name='v4l2_camera_node2',
             output="screen",
@@ -31,9 +28,6 @@ def generate_launch_description():
                 {
                     'video_device': '/dev/video2'
                 }
-            ],
-            remappings=[
-                ('image', 'image_raw2'),
             ],
             arguments=['--ros-args', '--log-level', 'info']
         )
